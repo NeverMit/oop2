db287a2e2e2aa0df79bdbd32319c1c3c7994faf2
NeverMit <106466574+NeverMit@users.noreply.github.com>
2023-02-25 12:39:24 +0500

    Add files via upload

---
+++ oop2/oop2.2(Creating methods).cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+class Human {
+protected:
+	string name;
+	int age;
+public:
+	Human()
+	{
+		cout << "Human()\n";
+		name = "Artem";
+		age = 20;
+
+	}
+	Human(string name, int age)
+	{
+		cout << "Human(string name, int age)\n";
+		this->name = name;
+		this->age = age;
+	}
+	Human(const Human& h)
+	{
+		cout << "Human(const Human &h)\n";
+		name = h.name;
+		age = h.age;
+	}
+	~Human()
+	{
+		cout << name << " " << age << "\n";
+		cout << "~Human()\n";
+
+	}
+	void addAge(int newAge)
+	{
+		age += newAge;
+	}
+	void changeName();
+};
+void Human::changeName()
+{
+	name = "Sasha";
+}
+int main()
+{
+	Human* h = new Human("Misha", 10);
+	h->addAge(10);
+	h->changeName();
+	delete h;
+}
